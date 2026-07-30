29a16317800392fad21ae8f61eaee2cfe7a03e90
Giottos <giottos330@gmail.com>
2026-07-30 23:25:05 +0200

    Final primera sesion2

---
+++ 0.0_Hello.py
@@ -0,0 +1,29 @@
+# Hola mundo, esto es un comentario
+print("Hello,World!")
+print('Hello,World!')
+
+
+"""
+Este es un 
+comentario 
+en varias 
+lineas
+"""
+
+'''
+Este es un 
+comentario 
+en varias 
+lineas con
+comillas
+simples
+'''
+
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+
