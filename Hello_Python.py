c25939bd877212f1db891ff0421ee580f8235a48
Giottos <giottos330@gmail.com>
2026-07-30 20:28:53 +0200

    Final primera sesion

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,1 +1,19 @@
-print("Hola")
+# Hola mundo, esto es un comentario
+print("Hello,World!")
+print('Hello,World!')
+
+"""
+Este es un 
+comentario 
+en varias 
+lineas
+"""
+
+'''
+Este es un 
+comentario 
+en varias 
+lineas con
+comillas
+simples
+'''
