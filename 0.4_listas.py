85f3deef1bbcc85870a5668a830df06919d12129
Giottos <giottos330@gmail.com>
2026-07-31 23:12:22 +0200

    Final segunda sesion2

---
+++ 0.4_listas.py
@@ -0,0 +1,18 @@
+### Listas ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35, 24, 62, 30, 17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [14, 1.83, "José", "Lara"]
+
+print(type(my_list))
+print(type(my_other_list))
+
+print(my_other_list[0])
