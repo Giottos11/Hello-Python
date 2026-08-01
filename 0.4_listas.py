fd9bfc5fb9f8b7273795e59f510407073d9b2431
Giottos <giottos330@gmail.com>
2026-08-01 14:53:00 +0200

    Final tercera sesion

--- 0.4_listas.py
+++ 0.4_listas.py
@@ -5,7 +5,7 @@ my_other_list = []
 
 print(len(my_list))
 
-my_list = [35, 24, 62, 30, 17]
+my_list = [35, 24, 62, 30, 30, 17]
 
 print(my_list)
 print(len(my_list))
@@ -16,3 +16,12 @@ print(type(my_list))
 print(type(my_other_list))
 
 print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-4])
+print(my_list.count(30))
+# print(my_other_list[4]) IndexError
+# print(my_other_list[-5]) IndexError
+
+age, height, name, surname = my_other_list
+print(name)
