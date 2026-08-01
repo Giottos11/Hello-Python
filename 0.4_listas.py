e0da4019c7eee36cf934aaaa85749feba5b37849
Giottos <giottos330@gmail.com>
2026-08-01 20:29:27 +0200

    Final tercera sesion2

--- 0.4_listas.py
+++ 0.4_listas.py
@@ -24,4 +24,45 @@ print(my_list.count(30))
 # print(my_other_list[-5]) IndexError
 
 age, height, name, surname = my_other_list
+
+
+surname = my_other_list[2], my_other_list[1], my_other_list[0], my_other_list[3]
 print(name)
+
+print(my_list + my_other_list)
+# print(my_list - my_other_list)
+
+my_other_list.append("Giottos33")
+print(my_other_list)
+
+my_other_list.insert(1,"Rojo")
+print(my_other_list)
+
+my_other_list[1] = "Azul"
+print(my_other_list)
+
+
+
+my_list.remove(30)
+print(my_list)
+
+print(my_list.pop())
+print(my_list)
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+print(my_list)
+
+del my_list[2]
+print(my_list)
+
+my_new_list = my_list.copy()
+
+my_list.clear()
+print(my_list)
+print(my_new_list)
+
+my_list = "Hola Python"
+print(my_list)
+print(type(my_list))
+
