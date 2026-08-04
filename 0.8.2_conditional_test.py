f1771191b2ee27ed63ce7ea147c2d578977d2c93
Giottos <giottos330@gmail.com>
2026-08-04 20:00:18 +0200

    Final sexta sesion

---
+++ 0.8.2_conditional_test.py
@@ -0,0 +1,6 @@
+my_string = input("Pon algo: ")
+
+if my_string:
+    print("Ok")
+else:
+    print("¿Tu eres tonto?")
--- 0.8_conditionals.py
+++ 0.8_conditionals.py
@@ -5,15 +5,23 @@ my_condition = True
 if my_condition: # Es lo mismo que if my_condition == True
     print("Se ejecuta la condición del if")
 
-my_condition = 5 * 2
+my_condition = 5 * 3
 
 if my_condition == 10:
     print("Se ejecuta la condición del segundo if")
 
-if my_condition >= 10:
-    print("Es mayor que 10")
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 25:
+    print("Es igual a 25")
 else:
-    print("Es menor o igual que 10")
-
+    print("Es menor o igual que 10 o mayor o igual que 20 o distinto de 25")
 
 print("La ejecucuión continua")
+
+my_string = "Mi cadena de texto"
+
+if my_string:
+    print("Mi cadena de texto no es vacía")
+else:
+    print("Mi cadena de texto es vacía")
