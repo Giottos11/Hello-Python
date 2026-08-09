339c92c8005f6e72f5a1ba3b9dda334ccc216387
Giottos <giottos330@gmail.com>
2026-08-09 14:54:21 +0200

    Final decima sesion

---
+++ Intermediate/0.2_challenges.py
@@ -0,0 +1,76 @@
+### Challenges ###
+
+"""
+EL FAMOSO "FIZZ BUZZ"
+Escribe un programa que muestre por consola (con un print) los
+números de 1 a 100 (ambos incluidos y con un salto de línea entre
+cada impresión), sustituyendo los siguientes:
+- Múltiplos de 3 por la palabra "fizz".
+- Múltiplos de 5 por la palabra "buzz".
+- Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+"""
+
+def fizzbuzz():
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
+
+fizzbuzz()
+
+"""
+¿ES UN ANAGRAMA?
+Escribe una función que reciba dos palabras (String) y retorne
+verdadero o falso (Bool) según sean o no anagramas.
+- Un Anagrama consiste en formar una palabra reordenando TODAS
+  las letras de otra palabra inicial.
+- NO hace falta comprobar que ambas palabras existan.
+- Dos palabras exactamente iguales no son anagrama.
+"""
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+        return False
+    return sorted(word_one.lower()) == sorted(word_two.lower())
+
+print(is_anagram("TomMarvoloRiddle", "IamLordVoldemort"))
+
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
+def fibonacci():
+
+    prev = 0
+    next = 1
+
+    for index in range(50):
+        print(prev)
+
+        fib = prev + next
+        prev = next
+        next = fib
+
+fibonacci()
+
+"""
+¿ES NÚMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imprime los números primos entre 1 y 100.
+"""
+def is_prime(number):
+    if number < 2:
+        return False
+    elif number % number == 0 and number % 1 == 0:
+        return True
+
+is_prime(1)
