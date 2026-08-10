d110e51b9c1774b56c7578bd662ba8523ff405d8
Giottos <giottos330@gmail.com>
2026-08-10 19:31:23 +0200

    Final sesion 11.2

--- Intermediate/0.7_regular_expressions.py
+++ Intermediate/0.7_regular_expressions.py
@@ -2,12 +2,64 @@
 
 import re
 
-my_string = "Esta es la lección 7: Expresiones Regulares"
+# match
+
+my_string = "Esta es la lección 7: Lección llamada Expresiones Regulares"
 my_other_string = "Esta no es la lección 6: Manejo de Ficheros"
 
 match = re.match("Esta es la lección", my_string, re.I)
 print(match)
-print(match.span())
+start, end = match.span()
+print(my_string[start:end])
 
-print(re.match("Esta es la lección", my_other_string))
-print(re.match("Expresiones Regulares", my_string))
+match = re.match("Esta no es la lección", my_other_string)
+#if not(match == None): # Otra forma de comprobar el None
+#if match != None: # Otra forma de comprobar el None
+if match is not None:
+    print(match)
+    start, end = match.span()
+    print(my_other_string[start:end])
+
+#print(re.match("Esta es la lección", my_other_string))
+
+# search
+
+search = re.search("lección", my_string, re.I)
+print(search)
+start, end = search.span()
+print(my_string[start:end])
+
+# findall 
+
+findall = re.findall("lección", my_string, re.I)
+print(findall)
+
+# split
+
+print(re.split(":", my_string))
+
+# sub
+
+print(re.sub("[l|L]ección", "LECCIÓN", my_string))
+print(re.sub("Expresiones Regulares", "RegEx", my_string))
+
+# Patterns
+
+pattern = r"[l|L]ección"
+print(re.findall(pattern, my_string))
+
+pattern = r"[l|L]ección|Expresiones"
+print(re.findall(pattern, my_string))
+
+pattern = r"[0-9]"
+print(re.findall(pattern, my_string))
+print(re.search(pattern, my_string))
+
+pattern = r"\d"
+print(re.findall(pattern, my_string))
+
+pattern = r"\D"
+print(re.findall(pattern, my_string))
+
+pattern = r"[l]."
+print(re.findall(pattern, my_string))
