72aecc48065027c5e600290203c21e6126cef3cb
Giottos <giottos330@gmail.com>
2026-08-13 20:18:50 +0200

    Final sesion 14.2

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Url local: http://127.0.0.1:8000
+
 @app.get("/")
 async def root():
     return "¡Hola FastAPI!"
 
+# Url local: http://127.0.0.1:8000/url
+
 @app.get("/url")
 async def root():
     return { "url_curso":"https://mouredev.com/python" }
+
+
+# Inicia el server: python -m uvicorn main:app --reload
+# Detener el server: CTRL+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Redocly: http://127.0.0.1:8000/redoc
---
+++ Backend/FastAPI/users.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+#Inicia el server: python -m uvicorn users:app --reload
+
+# Entidad user
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    url: str
+    age: int
+
+
+users_list = [User(id=1,name="José Juan", surname="Lara", url="https://github.com/Giottos11", age="33"),
+              User(id=2,name="Brais",surname="Moure",
+                   url="https://moure.dev",age=35),
+              User(id=3,name="Giottos",surname="Ciber",url="https://github.com/Giottos11",age=33)]
+
+
+@app.get("/usersjson")
+async def usersjson():
+    return [{"name":"José Juan", "surname":"Lara", "url":"https://github.com/Giottos11", "age":33},
+            {"name":"Brais", "surname":"Moure", 
+             "url":"https://moure.dev", "age":35},
+            {"name":"Giottos", "surname":"Ciber", "url":"https://github.com/Giottos11", "age":33}]
+
+
+@app.get("/users")
+async def users():
+    return users_list
+
+
+# Path
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+# Query
+
+
+@app.get("/user/")
+async def user(id: int):
+    return search_user(id)
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
