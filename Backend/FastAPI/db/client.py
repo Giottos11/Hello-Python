9c034dd9408cf55f3a1ba729b44b83be2a607d00
Giottos <giottos330@gmail.com>
2026-08-18 11:49:29 +0200

    Final sesion 19.2

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,5 +1,7 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://giottos330_db_user:QZtcfvdDvgtIdK4o@cluster0.iab2yrf.mongodb.net/?appName=Cluster0")
+client = MongoClient(os.getenv("MONGODB_URI"))
 
 db_client = client["users_db"]
+
